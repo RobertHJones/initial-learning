9d74abe09c37916a5b1eb61863ba5f922b0a3ca2
RobertHJones <roberthuwjones@outlook.com>
2021-04-05 14:58:26 +0100

    java

---
+++ Tribute/Java/JS.js
@@ -0,0 +1,16 @@
+//In-line comment
+/*Multi-line comment use camelCase
+see? */
+var thisBeVar;
+thisBeVar = 5;
+var moVar;
+moVar = thisBeVer;
+//but it's better to do it all in one line as below
+var a = 9;
+var b = 10;
+var c = "I am a";
+a = a + 1;
+b = b + 2;
+c = c + "man!";
+var add = 10 + 10;
+//can use + - * and /
