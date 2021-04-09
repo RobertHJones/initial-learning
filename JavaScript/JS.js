440a8fb78c873ffe77cda108a9b99e5a6d82344a
RobertHJones <roberthuwjones@outlook.com>
2021-04-09 17:31:41 +0100

    somoJS

--- JavaScript/JS.js
+++ JavaScript/JS.js
@@ -138,3 +138,17 @@ if (val >5) {
 } return result;
 } whoElse(6); /*like this. You can also chain if and else together
 if X return "X" else if Z return "Z" else return "Y"*/
+//below is example downloaded from FCC of chaining if else statements
+function testSize(num) {
+  if (num <5) {
+    return "Tiny"
+  } else if (num <10) {
+    return "Small"
+  } else if (num <15) {
+    return "Medium"
+  } else if (num <20) {
+    return "Large"
+  } else {
+    return "Huge";}
+  }
+  testSize(7);
